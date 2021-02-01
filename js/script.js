28a0a4aae8b3dd279ffd055ad9e1a6142a81c150
Switle <69729725+Switle@users.noreply.github.com>
2021-02-01 15:45:56 +0300

    +кнопка назад +кнопка в начало

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let narration = document.querySelector('.narration'),
     narrationBtn = document.querySelectorAll('.narration__btn'),
     questWrapper = document.querySelector('.questWrapper'),
-    startBtn = document.querySelector('.startBtn');
+    startBtn = document.querySelector('.startBtn'),
+    backBtn = document.querySelector('.backBtn'),
+    returnBtn = document.querySelector('.returnBtn');
 
 let textArr = ["Начало квеста",
-    "Второй элемент",
-    "Третий элемент",
+    "1",
+    "2",
+    "3",
     "4",
     "5",
     "6",
@@ -14,8 +17,7 @@ let textArr = ["Начало квеста",
     "9",
     "10",
     "11",
-    "12",
-    ""];
+    "12"];
 
 /* startBtn.onclick = function () { //не работает
     startBtn.textContent = 'Далее';
@@ -52,9 +54,45 @@ startBtn.onclick = function () { //Работает, и никакой цикл 
 function scene() {
     let i = 0;
     startBtn.onclick = function () { //Работает, и никакой цикл не нужен, чё я так парился
-        narration.textContent = textArr[i];
         i++;
+        narration.textContent = textArr[i];
         startBtn.textContent = 'Далее';
+        console.log(i);
+        if (i >= 1) {
+            backBtn.style.display = 'block';
+        }
+        if (i >= textArr.length) {
+            startBtn.style.display = 'none';
+        }
+        if (i >= textArr.length) {
+            returnBtn.style.display = 'block';
+        }
+
+    };
+
+    backBtn.onclick = function () {
+        i--;
+        narration.textContent = textArr[i];
+        console.log(i);
+        if (i < 1) {
+            backBtn.style.display = 'none';
+            startBtn.textContent = 'Старт';
+        }
+        if (i <= textArr.length) {
+            startBtn.style.display = 'block';
+        }
+    };
+
+    returnBtn.onclick = function () {
+        i = 0;
+        narration.textContent = textArr[i];
+        console.log(i);
+        if (i < 1) {
+            returnBtn.style.display = 'none';
+            backBtn.style.display = 'none';
+            startBtn.style.display = 'block';
+            startBtn.textContent = 'Старт';
+        }
     };
 }
 
